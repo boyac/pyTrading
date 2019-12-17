db20edc3e0d01d91dde7ed91754d912027333578
Boya Chiou <boya.chiou@gmail.com>
2019-12-17 14:30:46 +0800

    update money managment
    to be done: channel width, target price

--- pyTradeR.py
+++ pyTradeR.py
@@ -1,40 +1,45 @@
 # -*- coding: utf-8 -*-
 # @Author: Boya Chiou
-# @Date:   2018-03-22 17:16:22
-# @Last Modified by:   boyac
-# @Last Modified time: 2018-05-09 23:01:24
+# @Date: 2018-03-22 17:16:22
+# @Last Modified by: boyac
+# @Last Modified time: 2019-12-17 14:29:36
 
+import ffn
 
 class ScoreTrade(object):
 	"""docstring for ClassName"""
-	def __init__(self, ticker):
+	def __init__(self, ticker, principal):
 		self.ticker = ticker
+		self.principal = principal
+
 
 	def entry_score(self, high=0, low=0, bought=0):
-		"""進場分數(控制在50以下) 1 - High-Buy/(High-Low) * 100"""
+		"""進場分數(控制在50以下) 1 - (High - Buy)/(High - Low) * 100"""
 		self.bought = bought
-		entrys = (1-(high-self.bought)/(high-low))*100
+		entrys = (1 - (high - self.bought) / (high - low)) * 100
 		if entrys < 50:
-			print "Entry score = {} / Below 50, Good Job".format(entrys)
+			print "Entry score = {} / Below 50, Good Job".format(round(entrys, 2))
 		else:
-			print "Entry score = {} / Keep it below 50".format(entrys)
+			print "Entry score = {} / Keep it below 50".format(round(entrys, 2))
+
 
 	def exit_score(self, high=0, low=0, sold=0):
 		"""出場分數(控制在50以上) 1 - High-Sell/(High-Low) * 100"""
 		self.sold = sold
-		exits = (1 - (high-self.sold)/(high-low))*100
+		exits = (1 - (high - self.sold) / (high - low)) * 100
 		if exits > 50:
-			print "Exit score = {} / Above 50, Good Job".format(exits)
+			print "Exit score = {} / Above 50, Good Job".format(round(exits, 2))
 		else:
-			print "Exit score = {} / Keep it above 50".format(exits)
+			print "Exit score = {} / Keep it above 50".format(round(exits, 2))
+
 
 	def channel_score(self, channel_h=0, channel_l=0):
 		#self.bought = bought
 		#self.sold = sold
 		self.channel_h = channel_h
 		self.channel_l = channel_l
-		self.channel_width = round((self.channel_h-self.channel_l),1)
-		self.pctg = round((self.sold-self.bought)/self.channel_width,1)
+		self.channel_width = round((self.channel_h - self.channel_l), 1)
+		self.pctg = round((self.sold - self.bought) / self.channel_width, 1)
 
 		if self.pctg >= 0.3:
 			print "Channel Score: A"
@@ -44,55 +49,35 @@ class ScoreTrade(object):
 			print "Channel Score: C"
 		elif self.pctg < 0.1:
 			print "Channel Score: D" 
+		# print self.pctg # to confirm what the pctg means here 
 
-		print self.pctg
 
 	def target_price(self, fee=0, quantity=0):
-		self.target = 0.3*self.channel_width+self.bought+fee/quantity
-		print "{} / Target Price: {}".format(self.ticker, self.target)
+		self.tp = 0.3 * self.channel_width + self.bought + (fee / quantity)
+		print "{} / Target Price: {}".format(self.ticker, self.tp)
+
 
 	def equity_curve(self):
 		""""""
 		pass
 
-	def twopct_money(self, ticker=None, total=0, buy=0):
-		"""Date, account equity, 2%, 6%"""
-		self.ticker = ticker
-		self.total = total
-		self.buy = buy 
-		#self.stoploss = stoploss
-		self.stoploss = self.buy*0.9 #-10%
-		#self.position = (self.total*0.02)/(self.buy-self.stoploss)
-		self.position = (self.total*0.02)/(0.1*self.buy) # self.buy - 0.9*self.buy
-		print "{} / Allowed Position: {}".format(self.ticker, round(self.position))
 
-	def sixpct_money(self, ticker=None, total=0, buy=0, stock_num=0):
-		"""Date, account equity, 2%, 6%"""
-		self.ticker = ticker
-		self.total = total
-		self.buy = buy
-		self.stock_num = stock_num 
-		#self.stoploss = stoploss
-		self.stoploss = self.buy*0.9 #-10%
-		#self.position = (self.total*0.02)/(self.buy-self.stoploss)
-		self.position = (self.total*0.06/self.stock_num)/(0.1*self.buy) # self.buy - 0.9*self.buy
-		print "{} / Allowed Position: {}".format(self.ticker, round(self.position))
+	def mm(self, price=0, exposure=0.3, stoploss=0.4):
+		"""Money management, keep principal exposure to a single stock: 2%, 6%"""
+
+		threshold = exposure * stoploss
+		self.pos = (self.principal * exposure) / (price)
+		if threshold <= 0.06:
+			print "{0}, threshold:{1}, allowed position: {2}".format(self.ticker, threshold, round(self.pos))
+		else:
+			print "Exposure over Threshold!"
 
 
+	
 
 if __name__ == "__main__":
-	S = ScoreTrade('THD')
+	S = ScoreTrade('THD', 1000)
 
-	#How do I know the high and low of the day before I make a buy decision?
-	S.entry_score(high=104, low=95, bought=95)
-	#How do I know the high and low of the day before I make a sell decision?
-	S.exit_score(high=101, low=98, sold=100)
-
-	S.channel_score(channel_h=100, channel_l=82)
-	S.target_price(fee=2.95, quantity=10)
-	
-	#S.twopct_money(total=3000, buy=246)
-	#S.twopct_money(total=3000, buy=84)
 
 
 	"""
@@ -101,3 +86,13 @@ if __name__ == "__main__":
 	"""
 
 	print "-------------------BREAK-------------------"
+
+	dta = ffn.get('thd:Open, thd:High, thd:Low, thd:Close', start='2019-01-01')
+
+	highlow = dta.iloc[:,1:3]
+	print highlow
+	# for index, row in highlow.iterrows():
+	# 	print row, S.entry_score(row['pcghigh'], row['pcglow'], 3.8)
+
+	print S.mm(price=90, exposure=0.2, stoploss=0.2)
+
