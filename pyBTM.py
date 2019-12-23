1f10a0764f087bd0dccef8d83c8ae6b30bb745e7
Boya Chiou <boya.chiou@gmail.com>
2019-12-23 09:33:11 +0800

    init file

---
+++ pyBTM.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# @Author: boyac
+# @Date: 2019-12-23 08:15:15
+# @Last Modified by: boyac
+# @Last Modified time: 2019-12-23 09:30:52
+
+import ffn
+import bt
+
+class MF():
+	"""
+	Definition:
+		- earnings yield: =EBIT/EV, =EPS/Price (i) or PE's reciprocal
+		- return on capital: =(Net income - Dividens) / (Debt + Equity)
+
+	"""
+
+	pass
+
+
+
+if __name__ == "__main__":
+	pass 
