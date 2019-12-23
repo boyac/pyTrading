bcd6d69835cfd1dd8858abbcc9efd844489e7075
Boya邱柏雅 <Boya@cathaysite.com.tw>
2019-12-23 08:54:55 +0800

    init new file

---
+++ pyBTM.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# @Author: boyac
+# @Date: 2019-12-23 08:15:15
+# @Last Modified by: boyac
+# @Last Modified time: 2019-12-23 08:54:09
+
+import ffn
+import bt
+
+class MagicFormula():
+	"""
+	ref:
+		https://www.magicformulainvesting.com/
+
+	"""
+
+	pass
+
+
+
+if __name__ == "__main__":
+	pass
