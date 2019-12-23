b3ea04914c045ebbfb26c1b686f9a946d5d81eb3
boyac <n/a>
2019-12-23 09:26:49 +0800

    add definition descriptions

--- pyBTM.py
+++ pyBTM.py
@@ -2,15 +2,17 @@
 # @Author: boyac
 # @Date: 2019-12-23 08:15:15
 # @Last Modified by: boyac
-# @Last Modified time: 2019-12-23 09:04:10
+# @Last Modified time: 2019-12-23 09:26:18
 
 import ffn
 import bt
 
-class MagicFormula():
+class MF():
 	"""
-	ref:
-		https://www.magicformulainvesting.com/
+	Definition:
+		- earnings yield: =EBIT/EV, =EPS/Price (i) or PE's reciprocal
+		- return on capital: =(Net income - Dividens) / (Debt + Equity)
+
 
 	"""
 
